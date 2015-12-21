fcfbcf18f4258a10efe9627ec50e2a18f3d7f2e1
Karl Hughes <khughes.me@gmail.com>
2015-12-21 12:44:43 -0500

    Adding reactjs for templating and just because...

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+ReactDOM.render(
+    <div>
+        <h1>Jobs Common</h1>
+        <p>Easily integrate job board APIs into your PHP application</p>
+
+        <p class="view"><a href="https://github.com/JobBrander/jobs-common">View the Project on GitHub <small>JobBrander/jobs-common</small></a></p>
+
+        <ul>
+          <li><a href="https://github.com/JobBrander/jobs-common/zipball/master">Download <strong>ZIP File</strong></a></li>
+          <li><a href="https://github.com/JobBrander/jobs-common/tarball/master">Download <strong>TAR Ball</strong></a></li>
+          <li><a href="https://github.com/JobBrander/jobs-common">View On <strong>GitHub</strong></a></li>
+        </ul>
+    </div>,
+    document.getElementById('header')
+);
+
+ReactDOM.render(
+    <div>
+        <p>This project is maintained by <a href="https://github.com/JobBrander">JobBrander</a></p>
+        <p><small>Hosted on GitHub Pages &mdash; Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p>
+    </div>,
+    document.getElementById('footer')
+);
