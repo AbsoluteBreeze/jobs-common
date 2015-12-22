f69175c923af6b1185290b2cd4c2eae284170940
Karl Hughes <khughes.me@gmail.com>
2015-12-22 11:55:50 -0500

    Taking advantage of react for content

--- javascripts/providers.js
+++ javascripts/providers.js
@@ -1,17 +1,79 @@
+var body = document.getElementById('body');
+var providerName = body.getAttribute("providerName");
+var example = document.getElementById('example').textContent;
+
+// Repo/Packagist links
+var RepoLinks = React.createClass({
+    render: function() {
+        var githubLink = "https://github.com/JobBrander/jobs-" + providerName;
+        var packagistLink = "https://packagist.org/packages/JobBrander/jobs-" + providerName;
+        return (
+            <p>
+                <a href={githubLink} target="_blank">Github</a> | <a href={packagistLink} target="_blank">Packagist</a>
+            </p>
+        )
+    }
+});
+
+var linksElement = document.getElementById('links');
+if (linksElement) {
+    ReactDOM.render(
+      <RepoLinks />,
+      linksElement
+    );
+}
+                
+// Installation instructions
+var Installation = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h3>Installation</h3>
+                <p>Use composer:</p>
+                <pre>composer require jobbrander/jobs-{providerName}</pre>
+            </div>
+        )
+    }
+});
+
+var installationElement = document.getElementById('installation');
+if (installationElement) {
+    ReactDOM.render(
+      <Installation providerName={providerName} />,
+      installationElement
+    );
+}
+
 // Usage link
 var UsageLink = React.createClass({
     render: function() {
-        var url = "https://github.com/JobBrander/jobs-" + this.props.providerName + "#usage";
+        var url = "https://github.com/JobBrander/jobs-" + providerName + "#usage";
         return (
             <p>For more detailed usage and documentation, see <a href={url} target="_blank">this project's readme</a></p>
         )
     }
-})
+});
+
+var UsageDetails = React.createClass({
+    render: function() {
+        var url = "https://github.com/JobBrander/jobs-" + providerName + "#usage";
+        return (
+            <div>
+                <h3 id="usage">Usage</h3>
+                <p>Get the latest project manager jobs in Chicago:</p>
+                <pre>{example}</pre>
+            </div>
+        )
+    }
+});
 
 var usageElement = document.getElementById('usage');
 if (usageElement) {
     ReactDOM.render(
-      <UsageLink providerName={usageElement.getAttribute("providerName")} />,
+      <div>
+          <UsageDetails />
+          <UsageLink providerName={providerName} />
+      </div>,
       usageElement
     );
 }
@@ -19,7 +81,6 @@ if (usageElement) {
 // Demo Code HTML
 var DemoCode = React.createClass({
   render: function() {
-    var providerName = this.props.providerName;
     return (
         <div>
             <h3>Demo and Code Sample</h3>
@@ -31,9 +92,8 @@ var DemoCode = React.createClass({
 
 var DemoContent = React.createClass({
   render: function() {
-    var providerName = this.props.providerName;
-    var demoUrl = "http://demo.jobbrander.com/search/" + this.props.providerName;
-    if (providerName) {
+    var demoUrl = "http://demo.jobbrander.com/search/" + providerName;
+    if (demo == true) {
         return (
             <p>See a demo and code sample at <a href={ demoUrl } target='_blank'>demo.jobbrander.com/{ providerName }</a>.</p>
         );
@@ -48,7 +108,7 @@ var DemoContent = React.createClass({
 var demoCodeElement = document.getElementById('demo');
 if (demoCodeElement) {
     ReactDOM.render(
-      <DemoCode providerName={demoCodeElement.getAttribute("providerName")} />,
+      <DemoCode providerName={providerName} />,
       demoCodeElement
     );
 }
